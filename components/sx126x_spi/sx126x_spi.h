b6d0960762e2f6573e841a0710b3e26b4701d77c
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-02-24 13:30:07 +0100

    .

--- components/sx126x_spi/sx126x_spi.h
+++ components/sx126x_spi/sx126x_spi.h
@@ -4,18 +4,18 @@
 #include "esphome/components/spi/spi.h"
 
 namespace esphome {
-namespace sx126x_spi {
+    namespace sx126x_spi {
 
-class Sx126XSpiComponent : public Component {
+        class Sx126XSpiComponent : public Component {
 
-  public:
-    void setup() override;
-    void dump_config() override;
+            public:
+                void setup() override;
+                void dump_config() override;
 
-    float get_setup_priority() const override;
+                float get_setup_priority() const override;
 
-   protected:
-};
+            protected:
+        };
 
-}  // namespace sx126x_spi
+    }  // namespace sx126x_spi
 }  // namespace esphome

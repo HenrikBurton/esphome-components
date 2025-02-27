20cbb3d6066e13f33b61eb2254e7e26284629f1c
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-02-27 13:52:40 +0100

    .

--- components/sx126x_spi/sx126x_spi.h
+++ components/sx126x_spi/sx126x_spi.h
@@ -7,8 +7,8 @@ namespace esphome {
     namespace sx126x_spi {
 
         class Sx126XSpiComponent : public Component,
-                                   public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH, spi::CLOCK_PHASE_LEADING,
-                                                         spi::DATA_RATE_1MHZ> {
+//                                   public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH, spi::CLOCK_PHASE_LEADING,
+//                                                         spi::DATA_RATE_1MHZ> {
 
             public:
                 void setup() override;

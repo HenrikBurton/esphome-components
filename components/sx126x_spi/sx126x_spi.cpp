7082037159ff98ecc45306662f803f3bbb66a637
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-02-28 18:32:18 +0100

    .

--- components/sx126x_spi/sx126x_spi.cpp
+++ components/sx126x_spi/sx126x_spi.cpp
@@ -38,14 +38,14 @@ namespace esphome {
 
                 ESP_LOGD(TAG, "Read status");
                 uint8_t value = 0;
-                SPIDevice::begin_transaction();
+                this->delegate_->begin_transaction();
                 uint8_t command[] = {0xC0, 0x00};
                 write_array(command, sizeof(command));
                 //SPIDevice::write_byte16(0xC000);
                 //value = SPIDevice::read_byte() << 8;
                 //value |= SPIDevice::read_byte();
                 read_array(this->rx_buffer, sizeof(command));
-                SPIDevice::end_transaction();
+                this->delegate_->end_transaction();
                 value = this->rx_buffer[0] << 8 | this->rx_buffer[1];
                 ESP_LOGD(TAG, "read_register_: %d", value);
             }

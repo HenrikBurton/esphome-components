fce979fbb254d63e8ccdb105d67140db9441218a
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-02-28 18:37:08 +0100

    .

--- components/sx126x_spi/sx126x_spi.cpp
+++ components/sx126x_spi/sx126x_spi.cpp
@@ -40,7 +40,7 @@ namespace esphome {
                 uint8_t value = 0;
                 this->delegate_->begin_transaction();
                 uint8_t command[] = {0xC0, 0x00};
-                this->delegate_->transfer(command, this->rx_bugger, sizeof(command));
+                this->delegate_->transfer(command, this->rx_buffer, sizeof(command));
                 //SPIDevice::write_byte16(0xC000);
                 //value = SPIDevice::read_byte() << 8;
                 //value |= SPIDevice::read_byte();

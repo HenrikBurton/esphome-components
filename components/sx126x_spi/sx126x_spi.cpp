b3b7bd3acd3032adb3a4261947813c46455dfaa1
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-03-03 13:17:12 +0100

    .

--- components/sx126x_spi/sx126x_spi.cpp
+++ components/sx126x_spi/sx126x_spi.cpp
@@ -112,7 +112,7 @@ namespace esphome {
             }
           }
 
-          void sx126xcommand(uchar8_t *command, uchar8_t *response, length) {
+          void sx126xcommand(uint8_t *command, uint8_t *response, uint32_t length) {
             this->delegate_->begin_transaction();
             this->delegate_->transfer(command, response, length);
             this->delegate_->end_transaction();

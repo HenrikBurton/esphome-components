b6ce83a355ea55f52cf244151eba955c73bdd32c
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-03-04 14:26:36 +0100

    .

--- components/sx126x_spi/sx126x_spi.cpp
+++ components/sx126x_spi/sx126x_spi.cpp
@@ -60,7 +60,7 @@ namespace esphome {
                 ESP_LOGD(TAG, "Blink!");
                 this->led_blink();
                 uint8_t cmd5[] = {0x13, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };                        // GetStats
-                sx126xcommand(cmd5, this->rx_buffer, 2);
+                sx126xcommand(cmd5, this->rx_buffer, 4);
                 uint32_t value;
                 value = this->rx_buffer[2] << 8 | this->rx_buffer[3];
                 ESP_LOGD(TAG, "status: %d NbPktRX %d", this->rx_buffer[0] << 8 | this->rx_buffer[1], this->rx_buffer[2] << 8 | this->rx_buffer[3]);

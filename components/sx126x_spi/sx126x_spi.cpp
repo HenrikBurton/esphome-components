0944dd7b0f98ee20824eb24dfed63c1da572b595
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-03-04 18:01:20 +0100

    .

--- components/sx126x_spi/sx126x_spi.cpp
+++ components/sx126x_spi/sx126x_spi.cpp
@@ -40,6 +40,8 @@ namespace esphome {
             delay(3);
             uint8_t cmd0[] = {0xC0, 0x00 }; // GetStats
             sx126xcommand(cmd0, this->rx_buffer, 2);
+            uint8_t cmd9[] = {0x80, 0x01};                       // SeStandby, STDBY_XOSC
+            sx126xcommand(cmd1, this->rx_buffer, 2);
             uint8_t cmd1[] = {0x8A, 0x00};                       // Set packet type with protocol GFSK
             sx126xcommand(cmd1, this->rx_buffer, 2);
             uint8_t cmd2[] = {0x93, 0x20};                       // SetRxTxFallbackMode,  The radio goes into STDBY_RC mode after Tx or Rx

70998198127bd45265df1395ad855fd4fc9d77f2
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-03-11 11:01:26 +0100

    .

--- components/sx126x_spi/sx126x_spi.cpp
+++ components/sx126x_spi/sx126x_spi.cpp
@@ -200,8 +200,8 @@ namespace esphome {
           }
 
           int16_t Sx126XSpiComponent::standby(uint8_t mode) {
-            const uint8_t data[] = { RADIOLIB_SX126X_CMD_SET_STANDBY, mode };
-            return(sx126xcommand(&data[0], this->rx_buffer, 2));
+            const uint8_t data[2] = { RADIOLIB_SX126X_CMD_SET_STANDBY, mode };
+            return(sx126xcommand(data, this->rx_buffer, 2));
           }
 
           int16_t Sx126XSpiComponent::setBufferBaseAddress(uint8_t txBaseAddress, uint8_t rxBaseAddress) {
